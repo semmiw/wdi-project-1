65483fd07d2a45c7c977270dfe123604ccd38b65
Semmi <semmi.semmiw@gmail.com>
2019-02-17 22:59:37 +0000

    layout updates

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # wdi-project-1
+
+                    Drakes and Ladders
+
+Drakes and Ladders is a Drake-themed take on the classic children's board game, Snakes and Ladders. Players roll a die to determine how many squares to move their token. The token moves from the bottom of the board to the top left to right, then right to left on the next row and so on, until they reach the last square at the top of the board. The first player to reach "Home" or the last square wins the game.
+
+If a player's token lands at the bottom of a ladder, they can move to the top of the ladder, advancing toward the goal. If the player's token lands at the head of a drake-snake, they must move to the tail, distancing them further from the goal. Every time a player hits a ladder or a drake-snake, a fact appears. There are three levels, each level lasts three rounds.  
+
+## Main Features
+
+* The game should be playable for one player, playing against the computer. Level Three will include two players.
+* Fact Generator
+* Audio Effects for winning/losing, button interaction, hitting a "drake-snake" or ladder, rolling the dice, and bonus rounds.
+* The winner should be displayed when the game is over
+* A window should appear asking if the player would like to play again.
+* Animation: While player is moving across the board.
+* Design Animation: Responsive design takes advantage of video and audio.  
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,49 @@
-console.log('Hello!')
+document.addEventListener('DOMContentLoaded',() => {
+
+  //DEFINE ALL VARIABLES//
+
+  //Dice Sound//
+  const dice = document.querySelector('.dice')
+  console.log('dice',dice)
+
+  //Dice Rolling & Dice Landing on Random Number//
+
+  //Animate Drake Intro Header//
+  const animateDrakeImg =  document.querySelector('.animate-drakeintro')
+  animateDrakeImg.classList.add('animated', 'bounceInUp')
+  const animateDrakeH1 =  document.querySelector('.animate-drakeintroh1')
+  animateDrakeH1.classList.add('animated', 'bounceInRight')
+
+  //DEFINE EVENT LISTENERS//
+
+  //Dice Sound//
+  dice.addEventListener('click', playDice)
+
+  //Dice Rolling//
+  // dice.addEventListener('click', diceRoll)
+
+  //Dice Landing on Random Number//
+
+  //DEFINE ASSOCIATED FUNCTIONS//
+  //Dice Sound//
+  function playDice (){
+    const diceSound = document.querySelector('#diceaudio')
+    console.log('diceSound',diceSound)
+    diceSound.play()
+  }
+  //   //Dice Rolling//
+  //   function diceRoll () {
+  //   const diceRoll = document.querySelector('#')
+  //   diceRoll.
+  //   //callback	Optional. A function to be executed after the animation completes. To learn more about callback, please read our jQuery Callback chapter//*Use for triggering random dice number*
+  // }
+  //   //Dice Landing on Random Number//
+
+})
+
+
+// const diceSound = document.querySelector('#diceaudio')
+// diceSound.src = 'Sounds/Rolldicesound.wav'
+// dice.addEventListener('click',() => {
+//   diceSound.play()
+// })
