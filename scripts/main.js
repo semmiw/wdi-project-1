9744bb1ab778e090e184bd9ff151767585bf1b6a
Semmi <semmi.semmiw@gmail.com>
2019-02-18 19:24:52 +0000

    updated project1 grid changes

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,13 @@ document.addEventListener('DOMContentLoaded',() => {
   console.log('dice',dice)
 
   //Dice Rolling & Dice Landing on Random Number//
-  // const die1 = document.getElementById('#die1')
-  // const status = document.getElementById('#status')
   const die1 = document.querySelector('#die1')
   const status = document.querySelector('#status')
+
+  //Player movement on board after dice roll//
+  // const player1moving = document.querySelector('.drake-player1')
+
+  let playerPosition = 0
 
   //Animate Drake Intro Header//
   const animateDrakeImg =  document.querySelector('.animate-drakeintro')
@@ -21,56 +24,54 @@ document.addEventListener('DOMContentLoaded',() => {
 
   //DEFINE EVENT LISTENERS//
 
-  //Dice Sound//
+  //Dice Sound & producing a random number//
   dice.addEventListener('click', playDice)
 
-  //Dice Rolling & Landing on a Random Number//
-  // die1.addEventListener('click', rollDice)
+  //Player movement on board after dice roll//
+  // player1moving.addEventListener('click', playerMoving)
 
   //DEFINE ASSOCIATED FUNCTIONS//
 
-  //Dice Sound//
+  //Game Board Grid//
+  const $gameBoard = $('.board-game-container')
+  const numTiles = 100
+  function createBoard() {
+    for (let i = 0; i<numTiles; i++){
+      $gameBoard.append('<div class="square"></div>')
+    }
+    console.log($('board-game-container'))
+  }
+  createBoard()
+  const $squares = $('.square')
+  $squares.eq(playerPosition).addClass('drake-player1')
+
+  // adds class of drake to current position
+  function playerMoving() {
+    $squares.removeClass('drake-player1')
+    $squares.eq(playerPosition).addClass('drake-player1')
+  }
+
+  //Dice Sound & producing a random number//
+
   function playDice (){
     const diceSound = document.querySelector('.diceaudio')
     // console.log('diceSound',diceSound)
     diceSound.play()
-
     const d1 = Math.floor(Math.random() * 6) + 1
     const diceTotal = d1
+    playerPosition += diceTotal
     die1.innerHTML = d1
     status.innerHTML = 'You rolled '+diceTotal+'.'
     if(d1 === 6) {
       status.innerHTML += 'Reppn the 6ix! Roll again!'
     }
+    playerMoving()
   }
-
-
-  //Dice Rolling & Landing on Random Number//
-
-  // function rollDice() {
-  //   const d1 = Math.floor(Math.random() * 6) + 1
-  //   const diceTotal = d1
-  //   die1.innerHTML = d1
-  //   status.innerHTML = 'You rolled '+diceTotal+'.'
-  //   if(d1 === 6) {
-  //     status.innerHTML += 'Reppn the 6ix! Roll again!'
-  //   }
-
-
-
-
-  //   function diceRoll () {
-  //   const diceRoll = document.querySelector('#')
-  //   diceRoll.
-  //   //callback	Optional. A function to be executed after the animation completes. To learn more about callback, please read our jQuery Callback chapter//*Use for triggering random dice number*
-  // }
-  //   //Dice Landing on Random Number//
-
 })
 
+//Player movement on board after dice roll//
+//icon moves according to number produced//
+// Dice Landing on Random Number Animation//
 
-// const diceSound = document.querySelector('#diceaudio')
-// diceSound.src = 'Sounds/Rolldicesound.wav'
-// dice.addEventListener('click',() => {
-//   diceSound.play()
-// })
+//   //callback	Optional. A function to be executed after the animation completes. To learn more about callback, please read our jQuery Callback chapter//*Use for triggering random dice number*
+// }
