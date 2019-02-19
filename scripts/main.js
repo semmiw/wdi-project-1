fb51e0c63122bc2a99c85ce5609d0eb5a0e1d11d
Semmi <semmi.semmiw@gmail.com>
2019-02-19 13:58:32 +0000

    Upgraded project1 movement

--- scripts/main.js
+++ scripts/main.js
@@ -11,12 +11,13 @@ document.addEventListener('DOMContentLoaded',() => {
   const status = document.querySelector('#status')
 
   //Player movement on board after dice roll//
-  // const player1moving = document.querySelector('.drake-player1')
-
   let playerPosition = 99
   let compPosition = 99
 
-//make the dice animate after player1 moves. Then, make computer player move right after.
+  //Define Ladder for Movement and Placement//
+  const ladder = 96
+  //Define Snake for Movement and Placement//
+  const snake = 76
 
   //Animate Drake Intro Header//
   const animateDrakeImg =  document.querySelector('.animate-drakeintro')
@@ -25,16 +26,21 @@ document.addEventListener('DOMContentLoaded',() => {
   const animateDrakeH1 =  document.querySelector('.animate-drakeintroh1')
   animateDrakeH1.classList.add('animated', 'bounceInRight')
 
+  //Animate Snake and Ladder Images, Intro//
+  // const animateLadder
+  // const animateSnake
+
   //DEFINE EVENT LISTENERS//
 
   //Dice Sound & producing a random number//
   dice.addEventListener('click', playDice)
 
-
-  //Player movement on board after dice roll//
-  // player1moving.addEventListener('click', playerMoving)
-
   //DEFINE ASSOCIATED FUNCTIONS//
+  alert('Which Drake Would You Like to Be?')
+  //Intro to Game: Player Choice Window//
+  function ladderMove() {
+    if(playerPosition === 96 || compPosition === 96) {
+      alert('You go up!')
 
   //Game Board Grid//
   const $gameBoard = $('.board-game-container')
@@ -49,44 +55,77 @@ document.addEventListener('DOMContentLoaded',() => {
   const $squares = $('.square')
   $squares.eq(playerPosition).addClass('drake-player1')
   $squares.eq(compPosition).addClass('drake-player2')
+  $squares.eq(ladder).addClass('ladder-test')
+  $squares.eq(snake).addClass('snake-test')
 
-  // adds class of drake to current position
+  // Adds a class of Drake to current position
   function playerMoving() {
     $squares.removeClass('drake-player1')
     $squares.eq(playerPosition).addClass('drake-player1')
-    //make dice go again here//
   }
-
   function compMoving() {
     $squares.removeClass('drake-player2')
     $squares.eq(compPosition).addClass('drake-player2')
-
   }
-  //Dice Sound & producing a random number//
+  //Function for Moving up a Ladder//
+  function ladderMove() {
+    if(playerPosition === 96 || compPosition === 96) {
+      alert('You go up!')
+      $squares.removeClass('drake-player1')
+      $squares.removeClass('drake-player2')
+      //and move to new square//
+      $squares.eq(73).addClass('drake-player1')
+      $squares.eq(73).addClass('drake-player2')
+    }
+  }
+  function snakeMove() {
+    if(playerPosition === 76 || compPosition === 76) {
+      alert('You go down. Sad face.')
+      $squares.removeClass('drake-player1')
+      $squares.removeClass('drake-player2')
+      //and move to new square//
+      $squares.eq(95).addClass('drake-player1')
+      $squares.eq(95).addClass('drake-player2')
+    }
+  }
+  //function for checking winner, setting the win condition//
+  function checkWinner() {
+    if(playerPosition <= 0) {
+      alert('Player 1 Wins!')
+    } else if(compPosition <= 0) {
+      alert('Computer Wins!')
+    }
+  }
 
+  //Dice Sound & producing a random number//
   function playDice (){
     const diceSound = document.querySelector('.diceaudio')
     // console.log('diceSound',diceSound)
     diceSound.play()
-    const d1 = Math.floor(Math.random() * 6) + 1
-    const diceTotal = d1
-    playerPosition -= diceTotal
+    const d1 = rollDice()
+    const d2 = rollDice()
+    console.log('player rolled', d1)
+    console.log('computer rolled', d2)
+    playerPosition -= d1
+    compPosition -= d2
     die1.innerHTML = d1
-    status.innerHTML = 'You rolled '+diceTotal+'.'
-    if(d1 === 6) {
+    status.innerHTML = 'You rolled '+d1+'.'
+    if(d1 === 6 || d2 === 6) {
       status.innerHTML += 'Reppn the 6ix! Roll again!'
     }
-
     playerMoving()
-    compMoving()
-    // When it's not player1 turn it's computer's turn.
-    // if playerMoving() stops then compPosition uses playDice() function
-    //
+    setTimeout(() => compMoving(), 4000)
+    checkWinner()
+    ladderMove()
+    snakeMove()
+
+    function rollDice() {
+      return Math.floor(Math.random() * 6) + 1
+    }
   }
 })
 
-//Player movement on board after dice roll//
-//icon moves according to number produced//
+
 // Dice Landing on Random Number Animation//
 
 //   //callback	Optional. A function to be executed after the animation completes. To learn more about callback, please read our jQuery Callback chapter//*Use for triggering random dice number*
